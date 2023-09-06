3464d7455948edd15d72d6d3541736483543bf60
sj <sujung033131@gmail.com>
2023-09-06 21:47:59 +0900

    0906 js 기초

---
+++ app.js
@@ -0,0 +1,12 @@
+const a = 5;
+const b = 2;
+let myName = "sujung";
+
+console.log(a + b);
+console.log(a / b);
+console.log(a * b);
+console.log("hello " + myName);
+
+myName = "SongSujung";
+
+console.log("my new name is " + myName); 
