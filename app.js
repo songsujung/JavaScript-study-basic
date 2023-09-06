0b1ed8705cf2af9c93d305c5b593b08d1befc9ad
sj <sujung033131@gmail.com>
2023-09-07 02:32:28 +0900

    0907 Functions

--- app.js
+++ app.js
@@ -1,29 +1,52 @@
-/* 회원정보를 array 구조로 정리하면, 각 항목이 어떤 것을 의미하는 지를 알 수 없기 때문에 적합한 방법이 아님
-const user = ["sujung", 27, "song123", "sujung033131@gmail.com"] */
+/* Functions이 없다면, 아래와 같이 반복적인 코드를 일일히 작성해야함.
+console.log("hello. my name is sujung and I'm 27");
+console.log("hello. my name is yujin and I'm 28");
+console.log("hello. my name is suji and I'm 29");
+console.log("hello. my name is jiyoung and I'm 30");
+console.log("hello. my name is minjung and I'm 31"); */
 
-// Objects
-let user = {
-    name: "sujung",
-    age: 27,
-    id: "song123",
-    email: "sujung033131@gmail.com"
+// function : 어떤 코드를 캡슐화해서, 여러번 실행될 수 있게 해줌
+// sayHello를 실행할 때마다 {}안에 작성 된 것이 실행됨
+// 위 코드와 같은 결과를 내기 위해선 function을 사용할 때 인수(argument)가 필요
+// argument : function을 실행하는 동안 어떤 정보를 function에게 보낼 수 있는 방법
+// nameOfPerson, age이 인수
+function sayHello(nameOfPerson, age){
+    console.log("hello. my name is " + nameOfPerson + " and I'm " + age);
 }
 
-console.log(user); // 'sujung', 27, 'song123', 'sujung033131@gmail.com'
-console.log(user.name); // 'sujung'
-console.log(user.age); // 27
-console.log(user.id); // 'song123'
-console.log(user.email); // 'sujung033131@gmail.com'
+// 실행
+sayHello("sujung", 27); // hello. my name is sujung and I'm 27
+sayHello("yujin", 28); // hello. my name is yujin and I'm 28
+sayHello("suji", 29); // hello. my name is suji and I'm 29
 
-// Objects 안의 값 변경
-user.name = "SongSujung";
-user.age = user.age + 1;
-console.log(user); // 정상적으로 변경됨 'SongSujung', 28, 'song123', 'sujung033131@gmail.com'
 
-/* Objects 자체(const)를 변경하면 오류발생 - const는 업데이트할 수 없기 때문에
-user = "SongSujung";
-console.log(user); // 에러 */
+// 간단한 계산기 만들어보기
+function plus(a, b){
+    console.log(a + b);
+}
 
-// Objects 안에 값을 추가
-user.phoneNum = "010-1234-5678";
-console.log(user); // 'SongSujung', 28, 'song123', 'sujung033131@gmail.com', '010-1234-5678'
+function divide(firstNum, secondNum){
+    console.log(firstNum / secondNum);
+}
+
+// 실행
+plus(); // a, b 값이 비어도 실행은 되지만, NaN(Not a Number)이라는 좋지 않은 결과가 뜸
+plus(3, 4); // 7
+divide(6, 2) // 3
+
+
+// 응용해서 사용해보기(object.function 형식)
+// 회원 Object 생성하고, 그 안에 function추가
+const user = {
+    name: "sujung",
+    sayHello: function(otherPersonsName) {
+        console.log("hello!" + otherPersonsName + " nice to meet you!");
+    }
+}
+
+// 실행
+console.log(user.name); // sujung
+user.sayHello("yujin"); // hello! yujin nice to meet you!
+user.sayHello("suji"); // hello! suji nice to meet you!
+
+
