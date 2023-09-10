bd872885cc99d060ad85a86aa928f578f573461c
sj <sujung033131@gmail.com>
2023-09-11 00:51:09 +0900

    0911 querySelector, querySelectorAll

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
-// html에 접근해 특정 항목를 가져와 Javascript에서 수정해보자.
+// html에 접근해 특정 항목를 가져와 Javascript에서 수정해보자.-2 querySelector
 
-// html의 title이라는 id 요소를 가져와 title에 할당
-const title = document.getElementById("title");
+// querySelector로 hello className요소의 h1태그를 가져와 title에 할당(element가 한 개일때)
+// const title = document.querySelector(".hello h1"); // .은 클래스라는 뜻
 
-console.dir(title); // title object 안의 여러 요소들이 console에 표시됨
+// console.log(title);
 
-// Javascript에서 innerText, className을 수정해보자.
-title.innerText = "Got you!";
-title.className = "Hello!!!!!!!";
+// querySelectorAll로 조건에 부합하는 모든 hello className요소의 h1태그를 가져와 title에 할당(element가 여러 개일때)
+const title = document.querySelectorAll(".hello h1"); // .은 클래스라는 뜻
 
-console.log(title.id); // title
-console.log(title.innerText); // Got you! ( Grab me!에서 Got you!로 변경됨)
-console.log(title.className); // Hello!!!!!!! ( Hello에서 Hello!!!!!!!로 변경됨)
+console.log(title);
