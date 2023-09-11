e4d2f357b54e6bce293f78090a66fbedce6bf1c2
sj <sujung033131@gmail.com>
2023-09-11 22:51:49 +0900

    0911 Event를 listen하는 방법 (click event)

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
-// html에 접근해 특정 항목를 가져와 Javascript에서 수정해보자.-2 querySelector
+// event를 listen해보자.(click event)
+const title = document.querySelector("div.hello:first-child h1"); // html의 element를 가져오기
 
-// querySelector로 hello className요소의 h1태그를 가져와 title에 할당(element가 한 개일때)
-// const title = document.querySelector(".hello h1"); // .은 클래스라는 뜻
+// 이벤트 실행시 실행될 function 만들기
+function handTitleClick() {
+    console.log("title was clicked!")
+}
 
-// console.log(title);
-
-// querySelectorAll로 조건에 부합하는 모든 hello className요소의 h1태그를 가져와 title에 할당(element가 여러 개일때)
-const title = document.querySelectorAll(".hello h1"); // .은 클래스라는 뜻
-
-console.log(title);
+title.addEventListener("click", handTitleClick); // click event를 listen하고, 클릭 시 handTitleClick 함수를 실행
