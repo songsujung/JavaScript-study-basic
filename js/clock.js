8ce5ee29a3f135ab977c5ae1707b9d4d1f405001
sj <sujung033131@gmail.com>
2023-09-13 23:51:46 +0900

    0913 시계 시분초 형식 맞추기 padStart

--- js/clock.js
+++ js/clock.js
@@ -13,9 +13,15 @@ function sayHello() {
 */
 
 // 00:00:00 형식의 시계를 만들어보자.
+// 그리고, 1~9초도 01, 02, 03...처럼 형식에 맞춰서 출력되도록 만들자.(12:12:02)
+// padStart : String의 길이가 지정한 숫자 길이보다 부족하면 지정한 문자를 string 앞에 넣어준다.
 function getClock() {
     const date = new Date();
-    clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+    const hours = String(date.getHours()).padStart(2, "0"); 
+    const minutes = String(date.getMinutes()).padStart(2, "0");
+    const seconds = String(date.getSeconds()).padStart(2, "0"); // 초의 자릿수를 string으로 변환시킨 뒤, 2보다 적으면 앞에 0을 붙여준다. -> 8(자릿수가 1개)이면, 08
+    clock.innerText = `${hours}:${minutes}:${seconds}`;
+    //clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
 }
 
 getClock(); // 시계 바로실행(setInterval만 사용하면 바로 실행안되고 1초기다렸다가 뜨기 때문에)
