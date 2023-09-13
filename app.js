371884b0d6f46bc5c05d104e8bb42e8402b6b1d1
sj <sujung033131@gmail.com>
2023-09-13 18:10:29 +0900

    0913 Getting Username, 백틱

--- app.js
+++ app.js
@@ -1,25 +1,21 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
 
-const link = document.querySelector("a");
+// 위 const처럼 중요한 정보를 담은 게 아니고, string값만을 담은 것이라서 변수를 대문자로 작성(관습)
+const HIDDEN_CLASSNAME = "hidden";
 
-/* 이 function은 사실 입력값을 받아내려고만 하는 용도가 아니다. event라는 인자를 할당해 event object의 정보를 받기 위한 용도이며,
-event object 중 preventDefault라는 함수를 사용해서 브라우저의 기본동작을 막을 것이다. */
-// 이 경우 인자를 event로 작성하는 것이 관행이다. 인자를 할당하는 것 만으로도 js로부터 event의 정보를 받을 수 있다.
-function onLoginSubmit(event) {  
-    //const userName = loginInput.value; // 입력값 받기
-    //console.log(event); // event object의 정보를 확인(preventDefault를 확인)
+// 유저가 이름을 제출하면, 입력 후 클릭했을 때의 form(input창에 입력한 내용이 클릭을 해도 그대로 남아있는 것)을 없애보자.
+
+// preventDefault라는 함수를 사용해서 브라우저의 기본동작을 막고, 입력값을 받기
+// .hidden을 추가해서 form을 숨기고, 유저이름을 변수로 저장해주고 그 이름은 h1 안에 넣기 그리고 화면에 띄우기
+function onLoginSubmit(event) {
     event.preventDefault(); // submitdml 기본동작 막기
-    console.log(loginInput.value); // 입력값 받기
+    loginForm.classList.add(HIDDEN_CLASSNAME); // .hidden 추가 
+    const userName = loginInput.value; // 입력값 받기
+    // greeting.innerText = "Hello! " + userName; // h1태그의 innerText
+    greeting.innerText = `Hello! ${userName}`; // `문자 ${변수명}` 이 규칙으로 바로 위 코드와 같은 결과를 낼 수 있다. `백틱기호`
+    greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-// link의 기본동작인 html에서 입력한 주소로 페이지가 이동되는 것을 막아보자.
-// click의 event object는 mouse event이고, 사용자가 어느위치에서 클릭했는지와 같은 정보를 제공한다.
-function handleLinkClick(event) {
-    event.preventDefault();
-    console.dir(event); // 사용자가 클릭한 위치 포함 event의 정보를 확인
-}
-
-// submit event를 감지(form을 submit할 때 입력값을 받아냄, 하지만 enter나 login버튼을 누를 때 새로고침이 되는 현상(submit의 기본동작)은 남아있음.-preventDefault로 해결)
 loginForm.addEventListener("submit", onLoginSubmit);
-link.addEventListener("click", handleLinkClick)
