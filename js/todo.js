ec249871ea6a0e33198c1ec0a1eb729f7d543d69
sj <sujung033131@gmail.com>
2023-09-16 20:20:19 +0900

    0916 Todo 3 - array save (JSON.stringify)

--- js/todo.js
+++ js/todo.js
@@ -2,6 +2,17 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
+// newTodo가 그려질 때(paintTodo)마다 array에 push
+const toDos = [];
+
+// toDos array의 내용을 localStorage에 담기
+// localStorage에는 array가 array형식으로 저장되지 않는다. "a", "b", "c"이런식으로 저장됨
+// JSON.stringify로 array를 string타입으로 변환 후, localStorage에 string을 array의 모양으로 저장 ["a", "b", "c"]
+// JSON.stringify : array든 어떠한 js코드던 string으로 변환
+function saveToDos() {
+    localStorage.setItem("todos", JSON.stringify(toDos)); 
+}
+
 function deleteToDo(event) {
     const li = event.target.parentElement; // 클릭된 element(button)의 부모Element(li)에 접근
     li.remove(); // li를 삭제
@@ -23,7 +34,9 @@ function handleToDoSubmit(event) {
     event.preventDefault(); // 기본동작 막기
     const newTodo = toDoInput.value; // input에 입력된 값을 newTodo에 할당(새로운 변수에 값을 저장)
     toDoInput.value = "" // 엔터를 누르면 값이 입력되었던 input란이 비워짐(newTodo가 비워지지 않음 무관)
+    toDos.push(newTodo); // toDos array를 가져와서 newTodo를 push
     paintTodo(newTodo); // newTodo를 paintTodo에 넣어서 호출
+    saveToDos(); // toDo 저장
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
